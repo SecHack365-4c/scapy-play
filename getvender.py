c84f434c9bb897547e6fcbb1c2f4ad4cf27397b5
IsseTeruhi-uni <isse.teruhi220@mail.kyutech.jp>
2023-11-06 09:45:44 +0900

    macアドレスの正しい取得とvendor情報取得

--- getvender.py
+++ getvender.py
@@ -1,70 +1,66 @@
 import scapy.all as scapy
-from fingerprint import fingerprint
+
+# import argparse
+from scapy.all import ARP
+from manuf import manuf
+import nmap
+
+# def get_arguments():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-t", "--target", dest="target", help="Target IP/IP Range")
+#     options = parser.parse_args()
+#     return options
 
 
-def get_mac_addresses(network):
-    """
-    ネットワーク上のすべての機器のMacアドレスを取得する
-
-    Args:
-      network: スキャンするネットワークのIPアドレス
-
-    Returns:
-      スキャンされた機器のMacアドレスのリスト
-    """
-
+def scan(ip):
+    arp_request = ARP(pdst=ip)
+    arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request = scapy.ARP(pdst=network)
-    arp_response = scapy.ARP()
-
-    packets = scapy.srp(broadcast / arp_request, timeout=1, verbose=False)
-
-    mac_addresses = []
-    for packet in packets:
-        if isinstance(packet, scapy.ARP):
-            mac_addresses.append(packet.hwsrc)
-
-    return mac_addresses
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=10, verbose=True)[0]
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
 
-def get_device_info(mac_address):
-    """
-    Macアドレスから端末のベンダー情報とos情報を取得する
-
-    Args:
-      mac_address: 取得する端末のMacアドレス
-
-    Returns:
-      端末のベンダー情報とos情報の辞書
-    """
-
-    device_info = {}
-    vendor_info = fingerprint.get_vendor(mac_address)
-    os_info = fingerprint.get_os(mac_address)
-
-    device_info["vendor"] = vendor_info
-    device_info["os"] = os_info
-
-    return device_info
+def get_vendor_info(mac_address):
+    p = manuf.MacParser()
+    vendor = p.get_manuf(mac_address)
+    if vendor is None:
+        return "unknown"
+    return vendor
 
 
-def main():
-    """
-    同一ネットワークに接続されているすべての機器のMacアドレスを取得し、そこから端末のベンダー情報とos情報を調べる
-    """
-
-    network = "192.168.1.0/24"
-    mac_addresses = get_mac_addresses(network)
-    if mac_addresses:
-        for mac_address in mac_addresses:
-            device_info = get_device_info(mac_address)
-
-            print("Macアドレス: " + mac_address)
-            print("ベンダー: " + device_info["vendor"])
-            print("OS: " + device_info["os"])
-    else:
-        print("ネットワーク上にデバイスは検出されませんでした。")
+def get_os_info(mac_address):
+    return "unknown"
 
 
-if __name__ == "__main__":
-    main()
+def print_result(results_list):
+    print("IP\t\t\tMAC Address")
+    print("----------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+# options = get_arguments()
+# print(options.target)
+# scan_result = scan("192.168.11.0/24")
+scan_result = scan("131.206.239.0/24")
+if scan_result:
+    i = 0
+    for mac_address in scan_result:
+        i += 1
+        print("deveice" + str(i) + ":")
+        print("ip: " + mac_address["ip"])
+        print("mac: " + mac_address["mac"])
+        vender_info = get_vendor_info(mac_address["mac"])
+        os_info = get_os_info(mac_address["mac"])
+        print("ベンダー: " + vender_info)
+        print("OS: " + os_info + "\t")
+        print("")
+
+else:
+    print("ネットワーク上にデバイスは検出されませんでした。")
+# print_result(scan_result)
